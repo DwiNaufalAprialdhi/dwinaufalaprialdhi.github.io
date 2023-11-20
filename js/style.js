9556d5d2470765d9088f6e80952dfdd6cdbb55fa
dwinaufalaprialdhi <dwinaufalaprialdhi@gmail.com>
2023-11-20 14:10:51 +0700

    fix project image and fix navbar overlay

--- js/style.js
+++ js/style.js
@@ -12,3 +12,16 @@ function navToggleClose() {
     console.log('close')
     overlayMenu.classList.remove('right-0')
 }
+
+// Navigation Scroll
+const nav = document.querySelector('#navigation')
+const navTop = nav.offsetTop;
+
+function stickyNavigation() {
+    if(window.scrollY > navTop){
+        nav.classList.add('bg-[#323232B2]')
+    } else {
+        nav.classList.remove('bg-[#323232B2]')
+    }
+}
+window.addEventListener('scroll', stickyNavigation);
