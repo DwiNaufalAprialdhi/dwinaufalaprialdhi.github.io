ed308aa83f923b59b680909c0649331f66758d87
DwiNaufalAprialdhi <dwinaufalaprialdhi@gmail.com>
2023-11-21 16:50:48 +0700

    add aboutme in index

--- js/style.js
+++ js/style.js
@@ -33,3 +33,39 @@ function stickyNavigation() {
     }
 }
 window.addEventListener('scroll', stickyNavigation);
+
+// Hero Tab Active Class
+const tabBars = document.querySelectorAll(".aboutme-tab-menu")
+
+const biography = document.querySelector('.biography')
+const values = document.querySelector('.values')
+const skills = document.querySelector('.skills')
+
+tabBars.forEach(tabBar => {
+    tabBar.addEventListener('click',() =>{
+        document.querySelector('.aboutme-tab-menu-active')?.classList.remove('aboutme-tab-menu-active')
+        tabBar.classList.add('aboutme-tab-menu-active')
+    })
+})
+
+function bioTab() {
+    console.log('bio')
+    biography.classList.remove('hidden')
+    skills.classList.add('hidden')
+    values.classList.add('hidden')
+    
+}
+
+function valuesTab() {
+    console.log('val')
+    biography.classList.add('hidden')
+    skills.classList.add('hidden')
+    values.classList.remove('hidden')
+}
+
+function skillsTab() {
+    console.log('skills')
+    biography.classList.add('hidden')
+    skills.classList.remove('hidden')
+    values.classList.add('hidden')
+}
